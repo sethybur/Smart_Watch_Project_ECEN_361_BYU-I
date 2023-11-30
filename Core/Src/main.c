2cd6cd4638a1f7b3f7d24e24d8de0f3afeec8687
sethybur <sethburton80@gmail.com>
2023-11-29 20:33:26 -0700

    pull up

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -495,11 +495,17 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : B1_Pin heartRateInterrupt_Pin */
-  GPIO_InitStruct.Pin = B1_Pin|heartRateInterrupt_Pin;
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : heartRateInterrupt_Pin */
+  GPIO_InitStruct.Pin = heartRateInterrupt_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(heartRateInterrupt_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
